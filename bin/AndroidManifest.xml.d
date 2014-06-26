28800a29ccbb679c750e87cf44180c374b60eee7
akiniwa <shin.akiba8@gmail.com>
2014-06-26 17:36:55 +0900

    add gitignore

--- bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/shinya/Documents/workspace/org.question/bin/AndroidManifest.xml : \
-/Users/shinya/Documents/workspace/org.question/AndroidManifest.xml \
--- bin/questionnaire-debug-unaligned.apk.d
+++
@@ -1,5 +0,0 @@
-/Users/shinya/Documents/workspace/org.question/bin/questionnaire-debug-unaligned.apk : \
-/Users/shinya/Documents/workspace/org.question/bin/questionnaire.ap_ \
-/Users/shinya/Documents/workspace/org.question/bin/classes.dex \
-/Users/shinya/Documents/workspace/org.question/src/org/dom/hoge.csv \
-/Applications/adt-bundle-mac-x86_64/sdk/tools/support/annotations.jar \
